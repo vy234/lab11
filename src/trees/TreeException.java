686147eb01545f7403692d4b782b73f653a9873d
vy234 <131425330+vy234@users.noreply.github.com>
2023-05-13 22:23:56 -0700

    updated TreeException

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,10 @@
 package trees;
 
-public class TreeException extends ???
+public class TreeException extends TreeNode
 {
 	public TreeException(String s)
 	{
 		// Pass s into a superclass ctor.
+		super(s);
 	}
 }
